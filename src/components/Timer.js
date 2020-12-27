f58a645af35cf07b7223dbf428c47cc13039c0aa
Sawyer Carroll <sawyercarroll@gmail.com>
2020-12-27 01:46:35 -0600

    first commit

---
+++ src/components/Timer.js
@@ -0,0 +1,68 @@
+import React, { useState, useRef } from "react";
+import "./style.css";
+
+// const element = <FontAwesomeIcon icon={faClock} />
+const Countdown = (props) => {
+  const [timer, setTimer] = useState(props.customStart)
+  const [isActive, setIsActive] = useState(false)
+  const [isPaused, setIsPaused] = useState(false)
+  const increment = useRef(null)
+
+  const handleStart = () => {
+    setIsActive(true)
+    setIsPaused(true)
+    increment.current = setInterval(() => {
+      setTimer((timer) => timer - 1)
+    }, 1000)
+  }
+
+  const handlePause = () => {
+    clearInterval(increment.current)
+    setIsPaused(false)
+  }
+
+  const handleResume = () => {
+    setIsPaused(true)
+    increment.current = setInterval(() => {
+      setTimer((timer) => timer - 1)
+    }, 1000)
+  }
+
+  const handleReset = () => {
+    clearInterval(increment.current)
+    setIsActive(false)
+    setIsPaused(false)
+    setTimer(props.customStart)
+  }
+
+  const formatTime = () => {
+    const getSeconds = `0${(timer % 60)}`.slice(-2)
+    const minutes = `${Math.floor(timer / 60)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+    const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`
+  }
+
+  return (
+    <div className="app">
+      <h3>React Countdown</h3>
+      <div className='stopwatch-card'>
+        <p>{formatTime()}</p>
+        <div className='buttons'>
+          {
+            !isActive && !isPaused ?
+              <button onClick={handleStart}>Start</button>
+              : (
+                isPaused ? <button onClick={handlePause}>Pause</button> :
+                  <button onClick={handleResume}>Resume</button>
+              )
+          }
+          <button onClick={handleReset} disabled={!isActive}>Reset</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Countdown;
